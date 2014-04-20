0003e67e7d69b01d244ee8915cc41a022523efcb
eddietree <eddie@illogictree.com>
2014-04-21 00:32:24 +0900

    commited more layers

--- sunflower/drawer.js
+++ sunflower/drawer.js
@@ -159,15 +159,189 @@ function Flower()
 			//path1.rotate(0.3);
 
 			//path0.segments[0].point = posStart;
-			//path0.segments[1].point = posMiddle + dirOpp * Math.cos( time  + i) * 15.0;
+			path0.segments[1].point = posMiddle + dirOpp * Math.cos( time  + i) * 15.0;
 			//path0.segments[2].point = posEnd;
+			path0.rotate(1.0);
 			
 			path1.segments[0].point = posStart;
 			path1.segments[1].point = posMiddle - dirOpp * Math.cos( time  + i) * 15.0;
 			path1.segments[2].point = posEnd;
+		}
+	}
+}
+
+function Center()
+{
+	this.init = function()
+	{
+		var numPetals = 32;
+		var deltaAngle = 2 * Math.PI / numPetals;
+
+		var radiusStart = 0.0;
+		var radiusEnd = 70.0;
+
+		this.petals = new Array();
+
+		var center = new Point( view.viewSize.width/2, view.viewSize.height/2 );
 
 
-			path0.rotate(0.4);
+		var circlePath = new Path.Circle(center, 20);
+		//circlePath.strokeColor = 'black';
+		circlePath.fillColor = 'white';
+
+		var petalStyle = {
+				fillColor: '#4260A9',
+				strokeColor: '#96B1BF',
+				strokeWidth: 2,
+			};
+
+		for ( var i = 0; i < numPetals; i++ )
+		{
+			var angle = i * deltaAngle;
+
+			var dir = new Point( Math.cos(angle), Math.sin(angle) );
+			var dirOpp = new Point( dir.y, -dir.x );
+
+			var posStart = center + dir * radiusStart;
+			var posEnd = center + dir * radiusEnd;
+			var posMidle = (posStart+posEnd)*0.5;
+
+			var posSide1 = posMidle + dirOpp * 15.0;
+			var posSide2 = posMidle - dirOpp * 25.0;
+
+			var segments = [posStart, posSide1, posEnd ];
+			var path0 = new Path(segments);
+
+			path0.style = petalStyle;
+			//path0.closed = true;
+			path0.smooth();
+
+
+			var petal = 
+			{
+				dir : dir,
+				dirOpp : dirOpp,
+				index : i,
+				posStart : posStart,
+				posEnd : posEnd,
+
+				path0: path0,
+			};
+
+			this.petals.push(petal);
+		}
+	}
+
+	this.update = function()
+	{
+		var time = g_time * 6;
+
+		var center = new Point( view.viewSize.width/2, view.viewSize.height/2 );
+		var radius = Math.min( view.viewSize.width, view.viewSize.height ) * 0.5;
+		var radiusStart = radius * 0.2;
+		var radiusEnd = radius * 0.8;
+
+		for ( var i = 0; i < this.petals.length; i++ )
+		{
+			var petal = this.petals[i];
+
+			var path0 = petal.path0;
+			var path1 = petal.path1;
+			var dir = petal.dir;
+			var dirOpp = petal.dirOpp;
+
+			var posStart = petal.posStart;
+			var posEnd = petal.posEnd;
+
+			//var posStart = center + dir * radiusStart;
+			//var posEnd = center + dir * radiusEnd;
+			var posMiddle = (posStart+posEnd)*0.5;
+
+
+			//path1.rotate(0.3);
+
+			//path0.segments[0].point = posStart;
+			path0.segments[1].point = posMiddle + dirOpp * Math.cos( time  + i*0.1) * 5.0;
+			//path0.segments[2].point = posEnd;
+			path0.rotate(-1.0, posMiddle);
+			
+
+		}
+	}
+}
+
+function Outside()
+{
+	this.init = function()
+	{
+		var numPetals = 32;
+		var deltaAngle = 2 * Math.PI / numPetals;
+
+		var radiusStart = 50.0;
+		var radiusEnd = 700.0;
+
+		this.petals = new Array();
+
+		var center = new Point( view.viewSize.width/2, view.viewSize.height/2 );
+
+		var petalStyle = {
+				//fillColor: '#5A7504',
+				strokeColor: '#F4E237',
+				strokeWidth: 2,
+			};
+
+		for ( var i = 0; i < numPetals; i++ )
+		{
+			var angle = i * deltaAngle;
+
+			var dir = new Point( Math.cos(angle), Math.sin(angle) );
+			var dirOpp = new Point( dir.y, -dir.x );
+
+			var posStart = center + dir * radiusStart;
+			var posEnd = center + dir * radiusEnd;
+			var posMidle = (posStart+posEnd)*0.5;
+
+			var posSide1 = posMidle + dirOpp * 15.0;
+			var posSide2 = posMidle - dirOpp * 25.0;
+
+			var segments = [posStart, posSide2, posEnd, posSide1 ];
+			var path0 = new Path(segments);
+
+			path0.style = petalStyle;
+			//path0.closed = true;
+			//path0.smooth();
+
+
+			var petal = 
+			{
+				dir : dir,
+				dirOpp : dirOpp,
+				index : i,
+				posStart : posStart,
+				posEnd : posEnd,
+
+				path0: path0,
+			};
+
+			this.petals.push(petal);
+		}
+	}
+
+	this.update = function()
+	{
+		var time = g_time * 10;
+
+		var center = new Point( view.viewSize.width/2, view.viewSize.height/2 );
+		var radius = Math.min( view.viewSize.width, view.viewSize.height ) * 0.5;
+		var radiusStart = radius * 0.2;
+		var radiusEnd = radius * 0.8;
+
+		for ( var i = 0; i < this.petals.length; i++ )
+		{
+			var petal = this.petals[i];
+
+			var path0 = petal.path0;
+			path0.rotate(-1.0);
 		}
 	}
 }
@@ -175,7 +349,9 @@ function Flower()
 var g_core = new Core();
 g_core.init();
 //g_core.add( "linetest", new LineTest() );
+g_core.add( "outside", new Outside() );
 g_core.add( "flower", new Flower() );
+g_core.add( "center", new Center() );
 
 var g_time = 0.0;
 
