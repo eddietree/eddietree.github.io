c4887472c0ed55169b46ef9a8d349a919b5b6742
eddietree <eddie@illogictree.com>
2014-07-10 00:02:46 -0700

    grass segments

--- voxi/js/Grass.js
+++ voxi/js/Grass.js
@@ -4,21 +4,33 @@ function Grass()
 
 	this.initTerrain = function()
 	{
-		var colorFloor = 0x79A836;
-
-		/// generate geometry
-		var geo = new THREE.Geometry();
-
-		var numBlades = 800;
+		var numBlades = 500;
 		var maxDist = 20.0;
 		var yMin = -10.0;
 		var yMax = -5.0;
 		var bladeWidth = 0.6;
+		var numSegmentsPerBlade = 5;
+
+		//var numVertsPerBlade = ((numSegmentsPerBlade-1)*2 + 4);
+		var numVertsPerBlade = numSegmentsPerBlade * 6;
+		var numVertsTotal = numVertsPerBlade * numBlades;
+		var yHeight = yMax-yMin;
+
+		/// generate geometry
+		var geo = new THREE.BufferGeometry();
+
+		geo.addAttribute( 'position', Float32Array, numVertsTotal, 3 );
+		geo.addAttribute( 'color', Float32Array, numVertsTotal, 3 );
+
+		var positions = geo.attributes.position.array;
+		var colors = geo.attributes.color.array;
 
 		var yDiff = new THREE.Vector3(0.0, yMax-yMin, 0.0);
 
 		for ( var i = 0; i < numBlades; i ++ ) 
 		{
+			var bladeVertIndexOffset = i * numVertsPerBlade;
+
 			var rootPos = new THREE.Vector3( randFloat(-maxDist, maxDist), yMin, randFloat(-maxDist, maxDist) );
 			
 			var bladeAngle = randFloat(0.0, Math.PI*2.0);
@@ -30,31 +42,71 @@ function Grass()
 			var topPosLeft = rootPosLeft.clone().add(yDiff);
 			var topPosRight = rootPosRight.clone().add(yDiff);
 
-			geo.vertices.push( rootPosLeft );
-			geo.vertices.push( rootPosRight );
-			geo.vertices.push( topPosRight );
-			geo.vertices.push( topPosLeft );
+			var deltaY = yHeight / (numSegmentsPerBlade+1);
+			var deltaPos = new THREE.Vector3( 0.0, deltaY, 0.0 );
+
+			for ( var j = 0; j < numSegmentsPerBlade; j++ )
+			{
+				// color
+				var delta = j / numSegmentsPerBlade;
+				var r = 0.0;
+				var g = delta;
+				var b = 0.0;
+
+				var numFloatsPerSegment =  18;
+				var segmentVertIndexOffset = bladeVertIndexOffset + j*2;
+				var segmentIndexOffset = segmentVertIndexOffset * numFloatsPerSegment;
+
+				var pt0 = rootPosLeft.clone().add( deltaPos.clone().multiplyScalar(j) );
+				var pt1 = rootPosRight.clone().add( deltaPos.clone().multiplyScalar(j) );
+				var pt2 = rootPosRight.clone().add( deltaPos.clone().multiplyScalar(j+1) );
+				var pt3 = rootPosLeft.clone().add( deltaPos.clone().multiplyScalar(j+1) );
+
+				// po
+				positions[ segmentIndexOffset + 0 ] = pt0.x;
+				positions[ segmentIndexOffset + 1 ] = pt0.y;
+				positions[ segmentIndexOffset + 2 ] = pt0.z;
+				positions[ segmentIndexOffset + 3 ] = pt1.x;
+				positions[ segmentIndexOffset + 4 ] = pt1.y;
+				positions[ segmentIndexOffset + 5 ] = pt1.z;
+				positions[ segmentIndexOffset + 6 ] = pt2.x;
+				positions[ segmentIndexOffset + 7 ] = pt2.y;
+				positions[ segmentIndexOffset + 8 ] = pt2.z;
+
+				positions[ segmentIndexOffset + 9 ] = pt0.x;
+				positions[ segmentIndexOffset + 10 ] = pt0.y;
+				positions[ segmentIndexOffset + 11 ] = pt0.z;
+				positions[ segmentIndexOffset + 12 ] = pt2.x;
+				positions[ segmentIndexOffset + 13 ] = pt2.y;
+				positions[ segmentIndexOffset + 14 ] = pt2.z;
+				positions[ segmentIndexOffset + 15 ] = pt3.x;
+				positions[ segmentIndexOffset + 16 ] = pt3.y;
+				positions[ segmentIndexOffset + 17 ] = pt3.z;
+
+				colors[ segmentIndexOffset + 0 ] = r;
+				colors[ segmentIndexOffset + 1 ] = g;
+				colors[ segmentIndexOffset + 2 ] = b;
+				colors[ segmentIndexOffset + 3 ] = r;
+				colors[ segmentIndexOffset + 4 ] = g;
+				colors[ segmentIndexOffset + 5 ] = b;
+				colors[ segmentIndexOffset + 6 ] = r;
+				colors[ segmentIndexOffset + 7 ] = g;
+				colors[ segmentIndexOffset + 8 ] = b;
+
+				colors[ segmentIndexOffset + 9 ] = r;
+				colors[ segmentIndexOffset + 10 ] = g;
+				colors[ segmentIndexOffset + 11 ] = b;
+				colors[ segmentIndexOffset + 12 ] = r;
+				colors[ segmentIndexOffset + 13 ] = g;
+				colors[ segmentIndexOffset + 14 ] = b;
+				colors[ segmentIndexOffset + 15 ] = r;
+				colors[ segmentIndexOffset + 16 ] = g;
+				colors[ segmentIndexOffset + 17 ] = b;
+			}
 		}
 
-		// init faces
-		for ( var i = 0; i < numBlades; i ++ ) 
-		{
-			var colorFloor = new THREE.Color();
-			var cube_color_val = randFloat(0.5,1) ;
-			colorFloor.setRGB( 0.5, cube_color_val, 0.2 );
-
-			// face top
-			var face = new THREE.Face3( i*4, i*4+1, i*4+2 );
-			face.color = colorFloor;
-			geo.faces.push( face );
-
-			// face top
-			var face = new THREE.Face3( i*4, i*4+2, i*4+3 );
-			face.color = colorFloor;
-			geo.faces.push( face );
-		}
-
-		var vertexColorMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+		var vertexColorMaterial = new THREE.MeshBasicMaterial( 
+			{ vertexColors: THREE.VertexColors, side: THREE.DoubleSide } );
 
 	    this.meshTerrain = new THREE.Mesh(
 	        geo,
