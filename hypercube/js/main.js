e3800062567e504f84a7014c5ab37ad600f88180
eddietree <eddie@illogictree.com>
2014-09-16 00:38:36 -0700

    reorgznied hypercube

--- hypercube/js/main.js
+++ hypercube/js/main.js
@@ -1,55 +1,108 @@
 function resize()
 {
-     if(window.innerWidth != currentWindowWidth){
-     	renderer.resize(window.innerWidth-3, window.innerHeight-3);
+	var num_pixel_offset = 3;
+
+     if(window.innerWidth != g_currentWindowWidth){
+     	g_renderer.resize(window.innerWidth-num_pixel_offset, window.innerHeight-num_pixel_offset);
 
         //renderer.view.style.width = window.innerWidth + "px";
 		//renderer.view.style.height = window.innerHeight + "px";
-        currentWindowWidth = window.innerWidth-3;
+        g_currentWindowWidth = window.innerWidth-num_pixel_offset;
      }
 }
 
-var currentWindowWidth = 0;
+function MascotBody()
+{
+	this.init = function()
+	{
+		var texture = PIXI.Texture.fromImage("img/Body01.png");
+		this.obj = new PIXI.Sprite(texture);
+		this.obj.anchor.x = 0.5;
+		this.obj.anchor.y = 0.5;
 
-// create an new instance of a pixi stage
-var stage = new PIXI.Stage(0xFFACA1);
+		g_stage.addChild(this.obj);
+	}
 
-// create a renderer instance
-var renderer = new PIXI.WebGLRenderer(1, 1);//autoDetectRenderer(400, 300);
+	this.update = function()
+	{
+		this.obj.position.x = window.innerWidth*0.5;
+   	 	this.obj.position.y = window.innerHeight*0.5;
+   	 	this.obj.scale.x = 0.5;
+   	 	this.obj.scale.y = 0.5;
+	}
+}
 
-// add the renderer view element to the DOM
-document.body.appendChild(renderer.view);
+function MascotHead()
+{
+	this.init = function()
+	{
+		var texture = PIXI.Texture.fromImage("img/Avatar.png");
+		this.obj = new PIXI.Sprite(texture);
+		this.obj.anchor.x = 0.5;
+		this.obj.anchor.y = 0.5;
+		g_stage.addChild(this.obj);
+	}
 
-requestAnimFrame( animate );
+	this.update = function()
+	{
+		this.obj.position.x = window.innerWidth*0.5;
+   	 	this.obj.position.y = window.innerHeight*0.5;
+   	 	this.obj.rotation += 0.01;
+	}
+}
 
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage("img/Body01.png");
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
+function ObjManager()
+{
+	this.init = function()
+	{
+		this.objs = 
+		{
+			head:new MascotHead(),
+			body:new MascotBody()
+		};
 
-// center the sprites anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+		for (var property in this.objs ) {
+		    if (this.objs.hasOwnProperty(property)) {
+		        
+		        var obj = this.objs[property];
+		        obj.init();
+		    }
+		}
+	}
 
-// move the sprite t the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
+	this.update = function()
+	{
+		for (var property in this.objs ) {
+		    if (this.objs.hasOwnProperty(property)) {
+		        
+		        var obj = this.objs[property];
+		        obj.update();
+		    }
+		}
+	}
 
-stage.addChild(bunny);
+	this.get = function( a_name )
+	{
+		return this.objs[a_name];
+	}
+}
 
 function animate() {
 
     requestAnimFrame( animate );
     resize();
 
-    bunny.position.x = window.innerWidth*0.5;
-    bunny.position.y = window.innerHeight*0.5;
-
-    // just for fun, lets rotate mr rabbit a little
-    bunny.rotation += 0.01;
-    bunny.scale.x = 0.5;
-    bunny.scale.y = 0.5;
+    g_objs.update();
 
     // render the stage
-    renderer.render(stage);
+    g_renderer.render(g_stage);
 }
+
+var g_currentWindowWidth = 0;
+var g_stage = new PIXI.Stage(0xFFACA1);
+var g_renderer = new PIXI.WebGLRenderer(1, 1);//autoDetectRenderer(400, 300);
+document.body.appendChild(g_renderer.view);
+var g_objs = new ObjManager();
+g_objs.init();
+
+requestAnimFrame( animate );
