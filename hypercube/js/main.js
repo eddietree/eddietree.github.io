b39a1b758533f6532e593dc825915679e8811a59
eddietree <eddie@illogictree.com>
2014-09-17 01:22:11 -0700

    tonejs generative music

--- hypercube/js/main.js
+++ hypercube/js/main.js
@@ -69,6 +69,14 @@ var g_time = 0.0;
 
 g_stage.mousedown = function(mouseData){
 	g_objs.get("head").onClick(mouseData);
+
+	var posClick = mouseData.global;
+	var screenSize = { x:window.innerWidth, y:window.innerHeight };
+	var posClickNormalized = {x:posClick.x/screenSize.x, y:posClick.x/screenSize.y };
+	var index = Math.floor (g_scale_0.length * posClickNormalized.x );
+	var freq = g_scale_0[ index ];
+	//osc.setFrequency( posClick.x );
+	osc.setFrequency( freq );
 }
 
 g_stage.touchstart = function(mouseData){
@@ -80,3 +88,23 @@ var g_objs = new ObjManager();
 g_objs.init();
 
 requestAnimFrame( animate );
+
+// C E G B
+var g_scale_0 = [ 261.63, 329.63, 392.0, 493.88, 523.25, 659.25, 783.99, 987.77, 1046.50, 1318.51, 1567.98, 1975.53, 2093.00, 2637.02, 3135.96, 3951.07 ];
+
+var osc = new Tone.Oscillator(440, "triangle");
+var vibrato = new Tone.LFO(6, -25, 25);
+vibrato.start();
+
+// feedback
+var feedbackDelay = new Tone.PingPongDelay("8n");
+feedbackDelay.setFeedback(0.7);
+osc.connect(feedbackDelay);
+feedbackDelay.toMaster();	
+feedbackDelay.setWet(0.5);	
+
+//connect it to the output
+osc.toMaster();
+osc.setVolume(-10);
+vibrato.connect(osc.detune);
+osc.start();
