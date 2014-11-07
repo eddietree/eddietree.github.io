a0ee96b1e1c48f19e9f005a07e9b4b5273aa9528
eddietree <eddie@illogictree.com>
2014-11-07 13:06:51 -0800

    removed fps

--- ghosting13/js/Settings.js
+++ ghosting13/js/Settings.js
@@ -1,6 +1,6 @@
 var Settings = {
 	ShowProfileText : true,
-	ShowFPS: true,
+	ShowFPS: false,
 	StartProfileIndex: 0,
 	Autoplay: false, // will auto-cycle scenes
 	AutoplayTimeout: 10000, // how long to wait for timeout
