837ff55f4735212bb51b63c9145227858240e8b6
Eddie Lee <eddie@funktroniclabs.com>
2015-05-08 00:35:05 -0700

    class cleanup

--- reactor/src/scripts/main.js
+++ reactor/src/scripts/main.js
@@ -1,31 +1,6 @@
-var test = 422220;
-
-// This THREEx helper makes it easy to handle window resize.
-// It will update renderer and camera when window is resized.
-//
-// # Usage
-//
-// **Step 1**: Start updating renderer and camera
-//
-// ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
-//    
-// **Step 2**: Start updating renderer and camera
-//
-// ```windowResize.stop()```
-// # Code
-
-//
-
-/** @namespace */
 var THREEx	= THREEx 		|| {};
-
-/**
- * Update renderer and camera when the window is resized
- * 
- * @param {Object} renderer the renderer to update
- * @param {Object} Camera the camera to update
-*/
 THREEx.WindowResize	= function(renderer, camera){
+
 	var callback	= function(){
 		// notify the renderer of the size change
 		renderer.setSize( window.innerWidth, window.innerHeight );
@@ -33,41 +8,62 @@ THREEx.WindowResize	= function(renderer, camera){
 		camera.aspect	= window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
 	}
+
 	// bind the resize event
 	window.addEventListener('resize', callback, false);
-	// return .stop() the function to stop watching window resize
 	return {
-		/**
-		 * Stop watching window resize
-		*/
 		stop	: function(){
 			window.removeEventListener('resize', callback);
 		}
 	};
 }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var g_scene = new THREE.Scene();
+var g_camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var g_renderer = new THREE.WebGLRenderer();
 
-camera.position.z = 5;
+g_renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( g_renderer.domElement );
+
+var App = function (firstName) {
+  //this.firstName = firstName;
+  console.log('Person instantiated');
+
+  this.init = function()
+  {
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x00ffaf } );
+	var cube = new THREE.Mesh( geometry, material );
+	g_scene.add( cube );
+
+	g_camera.position.z = 5;
+	g_camera.position.x = 2;
+  };
+  this.update = function()
+  {
+  		//cube.rotation.x += 0.1;
+		//cube.rotation.y += 0.1;
+  };
+
+  this.draw = function()
+  {
+
+  };
+
+};
+
+var app = new App();
+app.init();
 
 var render = function () {
 	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-
-	renderer.render(scene, camera);
+	app.update();
+	g_renderer.render(g_scene, g_camera);
 };
 
 render();
-THREEx.WindowResize(renderer, camera);
+THREEx.WindowResize(g_renderer, g_camera);
