f166936c5acb181988f0a3bc6255aa63e2ffffe4
Eddie Lee <eddie@funktroniclabs.com>
2015-05-08 00:42:17 -0700

    wow app works

--- reactor/src/scripts/main.js
+++ reactor/src/scripts/main.js
@@ -23,59 +23,6 @@ var g_scene;
 var g_camera;
 var g_renderer;
 
-var App = function (firstName) {
-  //this.firstName = firstName;
-
-  console.log('Person instantiated');
-
-  this.init = function()
-  {
-  	// init scene/renderer
-	g_scene = new THREE.Scene();
-	g_camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-	g_renderer = new THREE.WebGLRenderer();
-	g_renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( g_renderer.domElement );
-
-	this.initObjs();
-
-	g_camera.position.z = 5;
-	g_camera.position.x = 2;
-  };
-
-  this.initObjs = function()
-  {
-	// cube
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ffaf } );
-	var cube = new THREE.Mesh( geometry, material );
-	g_scene.add( cube );
-  };
-
-  this.start = function()
-  {
-  	THREEx.WindowResize(g_renderer, g_camera);
-
-  		var render = function () {
-			requestAnimationFrame( render );
-			app.update();
-			app.draw();
-		};
-
-		render();
-  };
-
-  this.update = function()
-  {
-  		//cube.rotation.x += 0.1;
-		//cube.rotation.y += 0.1;
-  };
-
-  this.draw = function()
-  {
-  	g_renderer.render(g_scene, g_camera);
-  };
-};
 
 var app = new App();
 app.init();
