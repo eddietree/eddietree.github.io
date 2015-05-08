0578c2f7bab4c2db6b6892576111d576df51175a
Eddie Lee <eddie@funktroniclabs.com>
2015-05-08 00:24:30 -0700

    resized demo screen full!

--- reactor/src/scripts/main.js
+++ reactor/src/scripts/main.js
@@ -1,5 +1,51 @@
 var test = 422220;
 
+// This THREEx helper makes it easy to handle window resize.
+// It will update renderer and camera when window is resized.
+//
+// # Usage
+//
+// **Step 1**: Start updating renderer and camera
+//
+// ```var windowResize = THREEx.WindowResize(aRenderer, aCamera)```
+//    
+// **Step 2**: Start updating renderer and camera
+//
+// ```windowResize.stop()```
+// # Code
+
+//
+
+/** @namespace */
+var THREEx	= THREEx 		|| {};
+
+/**
+ * Update renderer and camera when the window is resized
+ * 
+ * @param {Object} renderer the renderer to update
+ * @param {Object} Camera the camera to update
+*/
+THREEx.WindowResize	= function(renderer, camera){
+	var callback	= function(){
+		// notify the renderer of the size change
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		// update the camera
+		camera.aspect	= window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+	}
+	// bind the resize event
+	window.addEventListener('resize', callback, false);
+	// return .stop() the function to stop watching window resize
+	return {
+		/**
+		 * Stop watching window resize
+		*/
+		stop	: function(){
+			window.removeEventListener('resize', callback);
+		}
+	};
+}
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
@@ -24,3 +70,4 @@ var render = function () {
 };
 
 render();
+THREEx.WindowResize(renderer, camera);
