c5625b7e478341489b8f5e6b151dbbfe5e0e90d2
Eddie Lee <eddie@funktroniclabs.com>
2015-08-21 22:51:05 -0700

    framework setup for shaders

--- ghosting13/js/SuperFormula.js
+++ ghosting13/js/SuperFormula.js
@@ -7,77 +7,6 @@ function SuperFormula()
 		this.time = 0.0;
 		renderer.setClearColor( 0xCCCCCC, 1);
 
-		var geometry = new THREE.BufferGeometry();
-		var material = new THREE.LineBasicMaterial({ vertexColors: THREE.VertexColors });
-
-		var positions = [];
-		var colors = [];
-		var indices_array = [];
-
-		var numPtsX = 96;
-		var numPtsY = 96;
-		var numVerts = numPtsX * numPtsY;
-		var deltaStep = 0.2;
-
-		// generate verts
-		var posXOffset = -numPtsX * deltaStep * 0.5;
-		var posYOffset = -numPtsY * deltaStep * 0.5;
-
-		for ( var y = 0; y < numPtsY; y++ )
-		{
-			for ( var x = 0; x < numPtsX; x++ )
-			{
-				var posX = posXOffset + x * deltaStep;
-				var posY = posYOffset + y * deltaStep;
-				var posZ = 0.0;
-
-				positions.push(posX, posY,posZ);
-				//colors.push(Math.random()*0.5+0.5, Math.random()*0.5+0.5, 1);
-				colors.push(0.65,0.65,0.65);
-				//colors.push(1.0,0.0,1.0);
-			}
-		}
-
-		// generate indices
-		for ( var y = 0; y < numPtsY-1; y++ )
-		{
-			var rowIndexOffset = y * numPtsX;
-
-			for ( var x = 0; x < numPtsX-1; x++ )
-			{
-				var indexCurr = rowIndexOffset + x;	
-				var indexRight = indexCurr + 1;	
-				var indexTop = indexCurr + numPtsX;	
-				var indexTopRight = indexTop + 1;	
-
-				indices_array.push(indexCurr, indexRight);	
-				indices_array.push(indexCurr, indexTop);	
-				indices_array.push(indexCurr, indexTopRight);	
-			}
-		}
-
-		// top row - needs to stitch together
-		for ( var x = 0; x < numPtsX-1; x++ )
-		{
-			var index0 = (numPtsY-1)*numPtsX + x;
-			var index1 = index0 + 1;
-			indices_array.push(index0, index1);
-		}
-
-		// right col - needs to stitch together
-		for ( var y = 0; y < numPtsY-1; y++ )
-		{
-			var index0 = y*numPtsX + numPtsX-1;
-			var index1 = index0 + numPtsX;
-			indices_array.push(index0, index1);
-		}
-
-		// vert attrib
-		geometry.addAttribute( 'index', new THREE.BufferAttribute( new Uint16Array( indices_array ), 1 ) );
-		geometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
-		geometry.addAttribute( 'color', new THREE.BufferAttribute( new Float32Array( colors ), 3 ) );
-		geometry.computeBoundingSphere();
-
 		var attributes = {
 			position: {	type: 'f', value: null },
 			color: { type: 'f', value: null },
@@ -91,17 +20,23 @@ function SuperFormula()
 		var shaderMaterial = new THREE.ShaderMaterial( {
 
 			uniforms: 		uniforms,
-			attributes:     attributes,
+			//attributes:     attributes,
 			vertexShader:   document.getElementById( 'vertexShaderSuperFormula' ).textContent,
-			fragmentShader: document.getElementById( 'fragmentShaderLines' ).textContent,
+			fragmentShader: document.getElementById( 'fragmentShaderSuperFormula' ).textContent,
 
 			//blending: 		THREE.AdditiveBlending,
 			depthTest: 		true,
 			transparent:	false,
 		});
 
+
+		var geometry = new THREE.SphereGeometry( 1, 96, 96 );
 		this.mesh = new THREE.Line( geometry, shaderMaterial, THREE.LinePieces );
 		scene.add( this.mesh );
+
+
+		//var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+		//var sphere = new THREE.Mesh( geometry, material );
 	};
 
 	this.release = function()
@@ -112,6 +47,12 @@ function SuperFormula()
 
 	this.update = function()
 	{
+		camera.position.x = 0;
+		camera.position.y = 0;
+		camera.position.z = 5;
+		camera.lookAt( new THREE.Vector3( 0, 0, -1 ) );
+
+
 		this.time += g_dt;
 		this.mesh.material.uniforms.time.value = this.time;
 	};
